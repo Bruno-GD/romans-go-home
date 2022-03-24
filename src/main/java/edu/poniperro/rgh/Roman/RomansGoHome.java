ebbbf244fde5f0640e47c046661fe6aa8d645625
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-03-24 17:58:29 +0100

    :recycle: Refactorizado RomansGoHome

--- src/main/java/edu/poniperro/rgh/Roman/RomansGoHome.java
+++ src/main/java/edu/poniperro/rgh/Roman/RomansGoHome.java
@@ -4,31 +4,46 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RomansGoHome {
-    private String numeroRomano;
+    private final String numeroRomano;
+    private Integer numeroDecimal = null;
+
+    public RomansGoHome(String numeroRomano) {
+        this.numeroRomano = numeroRomano;
+    }
 
     public String getNumeroRomano() {
         return numeroRomano;
     }
-
-    public void setNumeroRomano(String numeroRomano) {
-        this.numeroRomano = numeroRomano;
+    public Integer getNumeroDecimal() {
+        return numeroDecimal;
+    }
+    private void setNumeroDecimal(int numeroDecimal) {
+        this.numeroDecimal = numeroDecimal;
     }
 
     public int toDecimal() {
-        int decimal = 0;
+        if (getNumeroDecimal() == null)
+            setNumeroDecimal(0);
+        else
+            return getNumeroDecimal();
 
         int lastIndex = 0;
         for (int i = RomanNumber.values().length-1; i >= 0; i--) {
             RomanNumber rn = RomanNumber.values()[i];
             Pattern p = rn.getRegEx();
-            Matcher m = p.matcher(numeroRomano);
+            Matcher m = p.matcher(getNumeroRomano());
 
             if (m.find(lastIndex)) {
-                decimal += rn.getValue() * ((m.end() - m.start()) / rn.name().length());
+                int nuevoValor = getNumeroDecimal() +
+                        (rn.getValue() * ((m.end() - m.start()) / rn.name().length()));
+                setNumeroDecimal(nuevoValor);
                 lastIndex = m.end();
             }
+
+            if (lastIndex >= getNumeroRomano().length())
+                break;
         }
 
-        return decimal;
+        return getNumeroDecimal();
     }
 }
